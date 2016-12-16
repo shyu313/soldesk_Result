84e430b7693b3cf3fc035c78e7d2d79f5142aceb
ldch91 <ldch91@gmail.com>
2016-12-16 18:16:34 +0900

    가사집 크롤링 완료 / 유튜브 크롤링 조건찾기

--- test/src/main/java/kr/co/test/Test2.java
+++ test/src/main/java/kr/co/test/Test2.java
@@ -2,13 +2,13 @@ package kr.co.test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -19,6 +19,8 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Test2 {
 
@@ -32,7 +34,7 @@ public class Test2 {
 		System.out.println(" Start Date : " + getCurrentData());
 		int no = 1;
 
-		for (no = 1; no <= 1; no++) {
+		for (no = 30; no <= 30; no++) {
 			// 2. 가져올 HTTP 주소 세팅
 			HttpPost http = new HttpPost("http://gasazip.com/" + no + "");
 
@@ -67,32 +69,31 @@ public class Test2 {
 
 			// 11. Jsoup으로 파싱해보자.
 			Document doc = Jsoup.parse(sb.toString());
+			// StringBuffer str = new StringBuffer();
+
 			// System.out.println(doc.select("div.col-md-8"));
 			try {
-				String content = doc.select("div.col-md-8").toString(); // 제목, 가사 부분
-				String[] carray = content.split(">");
-				String subject = carray[2].substring(0, carray[2].length() - 4); // 제목 분리
-				String[] sarray = doc.select("div.col-md-4").toString().split(">"); // 가수 부분 추출
-				String[] ssarray = sarray[1].substring(3, sarray[1].length() - 7).split(" ");
-				String singer = ssarray[0]; // 가수 부분의 첫 번째 띄어쓰기 앞의 단어만 추출 -> 검색에 이용
-				// 예외처리
-				if(singer.equals("Unknown")) {
-					System.out.println(no+" is pass");
+				//String content = doc.select("div.col-md-8").toString(); // 제목, 가사 부분
+				//Elements contents = doc.select("div.col-md-8"); // 제목, 가사 부분
+
+				String subject = doc.select("div.col-md-8").get(0).text(); 	// 제목
+				String lyrics = doc.select("div.col-md-8").get(1).text(); 	// 가사
+				String metadata = doc.select("div.col-md-4").get(0).text();	// 가수
+				String singer = metadata.split(" ")[0].toString();
+				String album ="";
+				
+				if(metadata.contains("집")){									// 앨범 
+					album = metadata.substring(metadata.indexOf(":")+2, metadata.indexOf("집")+1);	// 공백제외
+				}
+				
+				if (metadata.contains("Unknown")) {							// 가수가 없는 경우 예외처리
+					System.out.println(no + " is pass");
 					continue;
 				}
-				// 앨범 '~집' 이란 단어가 있는 경우 album 값으로 저장
-				String album="";
-				for(int idx=0; idx<ssarray.length; idx++) {
-					if(ssarray[idx].equals("앨범")) {
-						for(int i=idx+2; i<ssarray.length; i++) {
-							if(ssarray[i].substring(ssarray[i].length()-1, ssarray[i].length()).equals("집")) {
-								album=ssarray[i];
-								break;
-							}
-						}
-						break;
-					}
-				}
+				// 가사집 크롤링 종료 
+				
+				
+				
 				
 				// youtube url 가져오기 (*함수형으로)
 				String searchSubject = subject.replace(" ", "+");
@@ -111,30 +112,50 @@ public class Test2 {
 				}
 				doc = Jsoup.parse(sb.toString());
 				// 결과 중 목록 형태가 아닌 하나의 영상이 연결된 링크 부분 가져오기
-				String[] varray = doc.select("div.contains-addto a").toString().split(" "); // contains-addto - 음악 재생 페이지
-				String resultValue="";
-				for(int idx=0; idx<varray.length; idx++) {
-					if(varray[idx].length()>6) {
-						if(varray[idx].substring(0,4).equals("href")) {
-							resultValue = varray[idx].substring(6, 26); // 링크의 주소부분 추출
+				
+				// 결과페이지의 title과 비교하기 위해 가수에 포함된 번호제거 
+				int index = subject.indexOf(".");
+				
+				
+				System.out.println(url);
+				Elements atag = doc.select("h3.yt-lockup-title a");
+				System.out.println(atag);
+				for(Element element: atag) {
+					if(element.attr("title").contains(subject)){
+						System.out.println("요청 타이틀 : " + subject );
+						System.out.println("응답 타이틀 : " +  element.attr("title"));
+						break;
+					}
+				}
+
+				/*String resultValue = "";
+				for (int idx = 0; idx < varray.length; idx++) {
+					if (varray[idx].length() > 6) {
+						if (varray[idx].substring(0, 4).equals("href")) {
+							resultValue = varray[idx].substring(6, 26); // 링크의
+																		// 주소부분
+																		// 추출
 						}
 					}
-					if(!resultValue.isEmpty()) // 링크주소를 얻고나면 빠져나오기
-						break; 
-				}
-				String sourceUrl = "https://www.youtube.com"+ resultValue; // 제일 첫번째 결과의 url
-				
-				BufferedWriter out = new BufferedWriter(new FileWriter("./lyrics/" + no + ".txt")); // 출력파일 만들기 : 번호.txt
+					if (!resultValue.isEmpty()) // 링크주소를 얻고나면 빠져나오기
+						break;
+				}*/
+				String sourceUrl = "https://www.youtube.com"; // 제일
+																			// 첫번째
+																			// 결과의
+																			// url
+
+				BufferedWriter out = new BufferedWriter(new FileWriter("./lyrics/" + no + ".txt")); // 출력파일
+																									// 만들기
+																									// :
+																									// 번호.txt
 				// 출력파일 내용
 				out.write("제목 : " + subject);
-				out.newLine(); 
-				out.write("가수 : " + sarray[1].substring(3, sarray[1].length() - 7));
 				out.newLine();
-				for (int i = 6; i < carray.length; i++) {
-					// 가사
-					out.write(carray[i].substring(0, carray[i].length() - 7));
-					out.newLine();
-				} 
+				out.write("가수 : " + singer);
+				out.newLine();
+				out.write(lyrics);
+				out.newLine();
 				out.write(sourceUrl);
 				out.close();
 			} catch (Exception e) {
