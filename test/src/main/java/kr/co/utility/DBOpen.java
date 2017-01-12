c559f5f3dd2302f0b9e920203942b9149c9bae88
all <all@DESKTOP-FBL64OU>
2017-01-12 16:29:04 +0900

    DB 붙이는 중
    1. 카페24 DB

--- test/src/main/java/kr/co/utility/DBOpen.java
+++ test/src/main/java/kr/co/utility/DBOpen.java
@@ -15,16 +15,16 @@ public class DBOpen {
 	public Connection getConnetion() {
 
 		// My-SQL DB
-		//String url = "jdbc:mysql://127.0.0.1:3306/mydb";
-		//String user = "root";
-		//String password = "1234";
-		//String driver = "org.gjt.mm.mysql.Driver";
+		String url = "umj64-006.cafe24.com/bardciel";
+		String user = "bardciel";
+		String password = "kobata11";
+		String driver = "org.gjt.mm.mysql.Driver";
 		
 		//ORACLE
-		String url = "jdbc:oracle:thin:@127.0.0.1:1521/xe";
+		/*String url = "jdbc:oracle:thin:@127.0.0.1:1521/xe";
 		String user = "java1230";
 		String password = "1234";
-		String driver = "oracle.jdbc.driver.OracleDriver";
+		String driver = "oracle.jdbc.driver.OracleDriver";*/
 
 		Connection con = null;
 
--- test/src/main/java/kr/co/utility/EmotionDictionary.java
+++ test/src/main/java/kr/co/utility/EmotionDictionary.java
@@ -3,4 +3,6 @@ package kr.co.utility;
 
 public class EmotionDictionary {
 	
+	
+	
 }
